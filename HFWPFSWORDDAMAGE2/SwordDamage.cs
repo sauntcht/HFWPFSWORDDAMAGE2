48a41c963a5c23199b27795be6e3dfde10744dc7
ehtom <ehtom@DESKTOP-SS1Q4KR>
2022-05-02 13:39:46 -0400

    Add project files.

---
+++ HFWPFSWORDDAMAGE2/SwordDamage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFWPFSWORDDAMAGE2
+{
+    internal class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        public decimal MagicMultiplier = 1m;
+        public int FlamingDamage = 0;
+        public int Damage;
+
+        public void CalcutlateDamage()
+        {
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+        }
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75m;
+            }
+            else
+            {
+                MagicMultiplier = 1m;
+            }
+        }
+        public void SetFlaming(bool isFlaming)
+        { 
+        CalcutlateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+        
+        }
+
+
+    }
+}
